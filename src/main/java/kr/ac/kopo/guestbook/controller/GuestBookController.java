c695094a70dca736b1143d26b562a2e738494f25
12YYCCYY16 <youtukr97@gmail.com>
2023-05-30 16:28:47 +0900

    0530

--- src/main/java/kr/ac/kopo/guestbook/controller/GuestBookController.java
+++ src/main/java/kr/ac/kopo/guestbook/controller/GuestBookController.java
@@ -10,6 +10,7 @@ import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -43,7 +44,7 @@ public class GuestBookController {
     @PostMapping("/register")
     public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes) {
 
-        log.info("dto..." + dto);
+        log.info("등록 처리 후 목록페이지 리다이렉트");
 
         // 새로 추가된 엔티티의 번호
         Long gno = service.register(dto);
@@ -53,5 +54,33 @@ public class GuestBookController {
         return "redirect:/guestbook/list";
     }
 
+    @GetMapping({"/read", "/modify"})
+    public void read(
+            long gno,
+            @ModelAttribute("requestDTO") PageRequestDTO requestDTO,
+            Model model
+    ) {
+        log.info("gno: " + gno);
+
+        GuestbookDTO dto = service.read(gno);
+
+        model.addAttribute("dto", dto);
+    }
+    @PostMapping("/remove")
+    public String remove(long gno, RedirectAttributes redirectAttributes){
+        log.info("gno:" + gno);
+        service.remove(gno);
+        redirectAttributes.addFlashAttribute("msg", gno);
+        return "redirect:/guestbook/list";
+
+    }
+    @PostMapping("/modify")
+    public String modify(GuestbookDTO dto, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, RedirectAttributes redirectAttributes ){
+        service.modify(dto);
+        redirectAttributes.addFlashAttribute("page", requestDTO.getPage());
+        redirectAttributes.addFlashAttribute("gno", dto.getGno());
+
+        return "redirect:/guestbook/read";
+    }
 }
 
